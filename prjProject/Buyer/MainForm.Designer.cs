6069b5e13e49ea2261ac15135279da67f97228b7
ShawnShen <a655059@hotmail.com.tw>
2022-09-07 17:20:07 +0800

    123

--- prjProject/Buyer/MainForm.Designer.cs
+++ prjProject/Buyer/MainForm.Designer.cs
@@ -88,14 +88,7 @@ namespace prjProject
             this.spContainerBotton.Panel1.SuspendLayout();
             this.spContainerBotton.Panel2.SuspendLayout();
             this.spContainerBotton.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
-            this.splitContainer1.Panel1.SuspendLayout();
-            this.splitContainer1.Panel2.SuspendLayout();
-            this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
-            this.splitContainer2.Panel1.SuspendLayout();
-            this.splitContainer2.Panel2.SuspendLayout();
-            this.splitContainer2.SuspendLayout();
+            
             ((System.ComponentModel.ISupportInitialize)(this.spContainerItem)).BeginInit();
             this.spContainerItem.Panel1.SuspendLayout();
             this.spContainerItem.Panel2.SuspendLayout();
@@ -684,14 +677,6 @@ namespace prjProject
             this.spContainerBotton.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.spContainerBotton)).EndInit();
             this.spContainerBotton.ResumeLayout(false);
-            this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
-            this.splitContainer1.ResumeLayout(false);
-            this.splitContainer2.Panel1.ResumeLayout(false);
-            this.splitContainer2.Panel2.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
-            this.splitContainer2.ResumeLayout(false);
             this.spContainerItem.Panel1.ResumeLayout(false);
             this.spContainerItem.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.spContainerItem)).EndInit();
